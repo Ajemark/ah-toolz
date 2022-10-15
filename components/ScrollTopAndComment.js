35d2d6ff284472c2579c9dbd24f328fd1f1e95bd
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-10-16 00:19:39 +0100

    Update ScrollTopAndComment.js

--- components/ScrollTopAndComment.js
+++ components/ScrollTopAndComment.js
@@ -4,11 +4,11 @@ import { useEffect, useState } from 'react'
 const ScrollTopAndComment = () => {
   const [show, setShow] = useState(false)
 
-//   useEffect(() => {
-//     const handleWindowScroll = () => {
-//       if (window.scrollY > 50) setShow(true)
-//       else setShow(false)
-//     }
+  useEffect(() => {
+    const handleWindowScroll = () => {
+      if (window.scrollY > 50) setShow(true)
+      else setShow(false)
+    }
 
     window.addEventListener('scroll', handleWindowScroll)
     return () => window.removeEventListener('scroll', handleWindowScroll)
